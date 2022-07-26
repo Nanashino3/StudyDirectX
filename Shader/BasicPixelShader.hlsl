b8dd25f732641a6b0bc7f924409a9cbb164f4901
ta.kusumoto <ta.kusumoto11827@gmail.com>
2022-07-27 00:31:03 +0900

    chapter5.3 complete

--- Shader/BasicPixelShader.hlsl
+++ Shader/BasicPixelShader.hlsl
@@ -1,8 +1,4 @@
-struct Input {
-	float4 pos:POSITION;
-	float4 svpos:SV_POSITION;
-};
-
-float4 BasicPS(Input input) : SV_TARGET{
-	return float4((float2(0,1) + input.pos.xy) * 0.5f,1,1);
+#include "BasicShaderHeader.hlsli"
+float4 BasicPS(S_OUTPUT input) : SV_TARGET{
+	return float4(input.uv, 1, 1);
 }
--- Shader/BasicVertexShader.hlsl
+++ Shader/BasicVertexShader.hlsl
@@ -1,11 +1,7 @@
-struct Output {
-	float4 pos:POSITION;
-	float4 svpos:SV_POSITION;
-};
-
-Output BasicVS(float4 pos : POSITION) {
-	Output output;
-	output.pos = pos;
+#include "BasicShaderHeader.hlsli"
+S_OUTPUT BasicVS(float4 pos : POSITION, float2 uv : TEXCOORD) {
+	S_OUTPUT output;
 	output.svpos = pos;
+	output.uv = uv;
 	return output;
 }
